02648999500b337086bc397e16fff999265af8c6
fjw1010 <fjw_1010@naver.com>
2024-07-24 22:32:13 +0900

    fix: 회원가입 페이지 기본 로직 짜는 중

--- src/app/(providers)/(root)/auth/signup/page.tsx
+++ src/app/(providers)/(root)/auth/signup/page.tsx
@@ -1,10 +1,39 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 function SingUpPage() {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
+  };
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  const onChangePasswordConfirm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirm(e.target.value);
+  };
+
+  // 새로고침 안 하게 하는 로직
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <main className="w-80 h-[1407px] relative overflow-hidden bg-white m-auto">
-      <form>
+      <form onSubmit={onSubmit}>
         <h1 className="absolute left-4 top-36 text-2xl font-medium text-center text-black">
           회원가입
         </h1>
@@ -14,6 +43,8 @@ function SingUpPage() {
           </label>
           <input
             type="text"
+            onChange={onChangeName}
+            value={name}
             placeholder="이름을 입력 해 주세요."
             className="self-stretch flex-grow-0 flex-shrink-0 h-[42px] opacity-50 rounded-lg bg-[#d9d9d9]"
           />
@@ -24,6 +55,8 @@ function SingUpPage() {
           </label>
           <input
             type="text"
+            onChange={onChangeNickname}
+            value={nickname}
             placeholder="닉네임을 입력해 주세요"
             className="self-stretch flex-grow-0 flex-shrink-0 h-[42px] opacity-50 rounded-lg bg-[#d9d9d9]"
           />
@@ -32,13 +65,20 @@ function SingUpPage() {
           <label className="self-stretch flex-grow-0 flex-shrink-0 w-72 text-lg text-left text-black">
             이메일
           </label>
-          <div className="flex justify-start items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5">
-            <input
-              type="email"
-              placeholder="이메일을 입력해 주세요"
-              className="flex-grow h-[42px] opacity-50 rounded-lg bg-[#d9d9d9]"
-            />
-          </div>
+          <input
+            type="email"
+            onChange={onChangeEmail}
+            value={email}
+            placeholder="이메일 아이디"
+            className="flex-grow h-[42px] opacity-50 rounded-lg bg-[#d9d9d9]"
+          />
+          <input
+            type="email"
+            onChange={onChangeEmail}
+            value={email}
+            placeholder="이메일 주소"
+            className="flex-grow h-[42px] opacity-50 rounded-lg bg-[#d9d9d9]"
+          />
         </div>
         <div className="flex flex-col justify-start items-start w-72 absolute left-4 top-[450px] pb-3">
           <label className="self-stretch flex-grow-0 flex-shrink-0 w-72 text-lg text-left text-black">
@@ -46,6 +86,8 @@ function SingUpPage() {
           </label>
           <input
             type="password"
+            onChange={onChangePassword}
+            value={password}
             placeholder="비밀번호"
             className="self-stretch flex-grow-0 flex-shrink-0 h-[42px] opacity-50 rounded-lg bg-[#d9d9d9]"
           />
@@ -56,6 +98,8 @@ function SingUpPage() {
           </label>
           <input
             type="password"
+            value={passwordConfirm}
+            onChange={onChangePasswordConfirm}
             placeholder="비밀번호 확인"
             className="self-stretch flex-grow-0 flex-shrink-0 h-[42px] opacity-50 rounded-lg bg-[#d9d9d9]"
           />
