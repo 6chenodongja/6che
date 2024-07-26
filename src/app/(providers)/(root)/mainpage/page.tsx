fc6c1b541520eb85c405d10912e96671d3b6b7e8
Jaeyoung <103552850+Wodud5683@users.noreply.github.com>
2024-07-26 15:43:16 +0900

    머지

--- src/app/(providers)/(root)/mainpage/page.tsx
+++ src/app/(providers)/(root)/mainpage/page.tsx
@@ -316,9 +316,9 @@ const MainPage = () => {
       </p>
       <div className="w-72 h-[46px] absolute left-[15px] top-[1200px] rounded-lg bg-[#d9d9d9]" />
       <Link href="/surveypage">
-      <p className="absolute left-[79px] top-[1210px] text-lg font-medium text-left text-black">
-        내 취향 코디 추천받기
-      </p>
+        <p className="absolute left-[79px] top-[1210px] text-lg font-medium text-left text-black">
+          내 취향 코디 추천받기
+        </p>
       </Link>
       <p className="absolute left-[266px] top-[990px] text-sm font-medium text-left text-black">
         더보기
