ad1d419578afbb8bbe85d1a9d530d7463f80d1d7
eunkyum <silverkyum27@naver.com>
2024-07-28 20:34:28 +0900

    fix: 버튼 사이즈 수정, supabase 연동

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,9 +1,29 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { Database } from "../../types/supabase";
+// import { createBrowserClient } from "@supabase/ssr";
+// import { Database } from "../../types/supabase";
 
-export function createClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+// export function createClient() {
+//   return createBrowserClient<Database>(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//   );
+// }
+
+// 7/28 18:19
+// import { createClient as createSupabaseClient } from '@supabase/supabase-js';
+// import { Database } from '../../types/supabase';
+
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// export const createClient = () => createSupabaseClient<Database>(supabaseUrl, supabaseAnonKey);
+
+
+// 7/28 18:46
+import { createClient as createSupabaseClient } from '@supabase/supabase-js';
+import { Database } from '../../types/supabase';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createSupabaseClient<Database>(supabaseUrl, supabaseAnonKey);
+export const createClient = () => createSupabaseClient<Database>(supabaseUrl, supabaseAnonKey);
