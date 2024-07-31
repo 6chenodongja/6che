d290bfb12841cc2a5fc4f2e95f2508b82aa819bc
eunkyum <silverkyum27@naver.com>
2024-07-31 09:57:46 +0900

    fix: 버튼 외 다른 영역 클릭 시 첫번째 버튼 클릭되는 오류 수정, 태그 추가 버튼 오류 수정

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -8,14 +8,6 @@
 //   );
 // }
 
-// 7/28 18:19
-// import { createClient as createSupabaseClient } from '@supabase/supabase-js';
-// import { Database } from '../../types/supabase';
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// export const createClient = () => createSupabaseClient<Database>(supabaseUrl, supabaseAnonKey);
 
 
 // 7/28 18:46
