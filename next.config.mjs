2d302cd94ad343ab3c7cab74fe102c7efa0e26ce
eunkyum <silverkyum27@naver.com>
2024-08-03 14:45:18 +0900

    dev pull

--- next.config.mjs
+++ next.config.mjs
@@ -1,49 +1,3 @@
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'image.msscdn.net',
-//       },
-//     ],
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'image.msscdn.net',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'developer.accuweather.com',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'image.msscdn.net',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'developer.accuweather.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'qnqowgdysxtsldlszczdcs.supabase.co',
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -53,11 +7,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'developer.accuweather.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'qn0wgdyssxts1dlszczdcs.supabase.co', // 여기에 오타가 있었네요.
+        hostname: 'qnowgdysxtsldlsczdcs.supabase.co',
       },
     ],
   },
