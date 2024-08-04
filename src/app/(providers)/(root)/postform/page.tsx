f0164b18b8faaf005a2baef344b2b79cc5fa5d0e
eunkyum <silverkyum27@naver.com>
2024-08-04 16:10:26 +0900

    feat: Add Header and Footer

--- src/app/(providers)/(root)/postform/page.tsx
+++ src/app/(providers)/(root)/postform/page.tsx
@@ -1,12 +1,18 @@
-"use client";
+'use client';
 
-import React from "react";
-import PostFormPage from "./PostForm";
+import React from 'react';
+import PostFormPage from './PostForm';
+import Header from 'app/(providers)/(components)/Header';
+import Footer from 'app/(providers)/(components)/Footer';
 
 function Page() {
   return (
     <div>
-      <PostFormPage />
+      <Header />
+      <main>
+        <PostFormPage />
+      </main>
+      <Footer />
     </div>
   );
 }
