1d82fd8607c6f63a563b25a8f4e5dda1ed187025
fjw1010 <fjw_1010@naver.com>
2024-08-05 20:58:59 +0900

    chore: 임시 커밋

--- src/app/(providers)/(root)/(auth)/login/_components/LoginForm.tsx
+++ src/app/(providers)/(root)/(auth)/login/_components/LoginForm.tsx
@@ -68,88 +68,88 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit} className="container h-auto justify-center">
-      <h1 className="mt-[119px] text-[24px] font-bold text-center text-[#121212]">
-        로그인
-      </h1>
-      <div className="bg-slate-50 flex flex-col">
-        <label
-          htmlFor="email"
-          className="text-[16px] font-bold text-left text-[#4d4d4d] mb-1 ml-1 flex"
-        >
-          이메일
-        </label>
-        <input
-          type="email"
-          id="email"
-          placeholder="이메일을 입력해 주세요."
-          ref={emailRef}
-          className="flex justify-start items-center flex-grow relative gap-2 px-4 py-3 rounded-xl border-2 border-[#808080]"
-        />
-      </div>
-      <div className="mt-2">
-        <label className="text-[16px] font-bold text-left text-[#4d4d4d] mb-1 ml-1 flex">
-          비밀번호
-        </label>
-        <input
-          type="password"
-          ref={passwordRef}
-          id="password"
-          placeholder="비밀번호를 입력해 주세요."
-          className="flex justify-start items-center flex-grow relative gap-2 px-4 py-3 rounded-xl border-[1px] border-[#808080]"
-        />
-      </div>
-      <div className="flex flex-col gap-2 bg-red-600">
-        <div className="flex flex-row w-full mt-16">
-          <button className="bg-black text-[#fff] rounded-xl px-4 py-3 w-[288px] font-bold">
-            로그인
+    <main>
+      <form onSubmit={onSubmit} className="container h-auto justify-center">
+        <h1 className="mt-[119px] text-[24px] font-bold text-center text-[#121212]">
+          로그인
+        </h1>
+        <div className="bg-slate-50 flex flex-col">
+          <label
+            htmlFor="email"
+            className="text-[16px] font-bold text-left text-[#4d4d4d] mb-1 ml-1 flex"
+          >
+            이메일
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="이메일을 입력해 주세요."
+            ref={emailRef}
+            className="flex justify-start items-center flex-grow relative gap-2 px-4 py-3 rounded-xl border-[1px] w-[288px] border-[#808080]"
+          />
+        </div>
+        <div className="mt-2">
+          <label className="text-[16px] font-bold text-left text-black-500 mb-1 ml-1 flex">
+            비밀번호
+          </label>
+          <input
+            type="password"
+            ref={passwordRef}
+            id="password"
+            placeholder="비밀번호를 입력해 주세요."
+            className="flex justify-start items-center flex-grow relative gap-2 px-4 py-3 rounded-xl border-[1px] w-[288px] border-[#808080]"
+          />
+        </div>
+        <div className="">
+          <input
+            type="checkbox"
+            placeholder="체크 박스입니다"
+            className="w-[18px] h-[18px] rounded bg-[#ccc]/70"
+          />
+          <label className="flex-grow-0 flex-shrink-0 text-sm text-left text-[#808080]">
+            로그인 유지
+          </label>
+        </div>
+        <div className="flex flex-col gap-2 bg-red-600">
+          <div className="flex flex-row w-full mt-16">
+            <button className="bg-black text-[#fff] rounded-xl px-4 py-3 w-[288px] font-bold">
+              로그인
+            </button>
+          </div>
+          <Link href={'/signup'} passHref>
+            <button className="bg-white border-2 border-[#808080] text-[#4d4d4d] rounded-xl px-4 py-3 w-[288px] font-bold">
+              회원가입
+            </button>
+          </Link>
+        </div>
+        <div className="flex-grow-0 flex-shrink-0 text-sm text-left text-[#4d4d4d]">
+          <Link href={''} className="">
+            아이디/비밀번호 찾기
+          </Link>
+        </div>
+        <div className="flex justify-start items-center w-72 relative gap-3 m-auto">
+          <div className="flex-grow h-px bg-[#d9d9d9]" />
+          <p className="flex-grow-0 flex-shrink-0 text-base text-left text-black">
+            or
+          </p>
+          <div className="flex-grow h-px bg-[#d9d9d9]" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <button
+            onClick={() => handleSocialLogin('google')}
+            className="bg-yellow-950 text-[#fff] w-[288px] rounded-xl"
+          >
+            구글
+          </button>
+          <button
+            onClick={() => handleSocialLogin('kakao')}
+            className="bg-yellow-950 text-[#fff] w-[288px] rounded-xl"
+          >
+            카카오톡
           </button>
         </div>
-        <Link href={'/signup'} passHref>
-          <button className="bg-white border-2 border-[#808080] text-[#4d4d4d] rounded-xl px-4 py-3 w-full font-bold">
-            회원가입
-          </button>
-        </Link>
-      </div>
-      {/* <div className="flex-grow-0 flex-shrink-0 text-sm text-left text-[#4d4d4d]">
-        <Link href={''} className="">
-          아이디/비밀번호 찾기
-        </Link>
-      </div> */}
-      <div className="flex justify-start items-center w-72 relative gap-3 m-auto">
-        <div className="flex-grow h-px bg-[#d9d9d9]" />
-        <p className="flex-grow-0 flex-shrink-0 text-base text-left text-black">
-          or
-        </p>
-        <div className="flex-grow h-px bg-[#d9d9d9]" />
-      </div>
-      <div className="flex flex-col gap-2">
-        <button
-          type="button"
-          onClick={() => handleSocialLogin('google')}
-          className=""
-        >
-          구글
-        </button>
-        <button
-          type="button"
-          onClick={() => handleSocialLogin('kakao')}
-          className=""
-        >
-          카카오톡
-        </button>
-      </div>
-      {/* <div className="">
-        <input
-          type="checkbox"
-          placeholder="체크 박스입니다"
-          className="w-[18px] h-[18px] rounded bg-[#ccc]/70"
-        />
-        <label className="flex-grow-0 flex-shrink-0 text-sm text-left text-[#808080]">
-          로그인 유지
-        </label>
-      </div> */}
-    </form>
+      </form>
+    </main>
   );
 };
 
