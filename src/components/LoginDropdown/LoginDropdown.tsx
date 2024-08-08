053b697e032c5f3832eb83fd5f3806b440a815fb
fjw1010 <fjw_1010@naver.com>
2024-08-08 19:03:44 +0900

    fix: 도메인 추가

--- src/components/LoginDropdown/LoginDropdown.tsx
+++ src/components/LoginDropdown/LoginDropdown.tsx
@@ -13,6 +13,7 @@ function LoginDropdown() {
   return (
     <div className="inline-block text-left">
       <button
+        title="프로필 아이콘"
         onClick={toggleDropdown}
         className="flex items-center justify-between bg-white border border-gray-300 rounded-lg shadow-sm px-4 py-2 text-sm font-medium text-gray-700 "
       >
