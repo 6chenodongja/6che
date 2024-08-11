28034ac4465b4cafec83111123a85e346caa6016
eunkyum <silverkyum27@naver.com>
2024-08-11 21:21:27 +0900

    feat: 바텀시트

--- src/app/(providers)/(root)/mainpage/components/BottomSheet.tsx
+++ src/app/(providers)/(root)/mainpage/components/BottomSheet.tsx
@@ -1,34 +1,35 @@
-// BottomSheet.tsx
-
 import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import Image from 'next/image';
 
 interface BottomSheetProps {
   onClose: () => void;
-  onSelect: (selectedElements: string[]) => void;
+  onSelectElements: (elements: string[]) => void;
   selectedElements: string[];
 }
 
 const BottomSheet: React.FC<BottomSheetProps> = ({
   onClose,
-  onSelect,
+  onSelectElements,
   selectedElements,
 }) => {
-  const [selected, setSelected] = useState<string[]>(selectedElements);
+  const [localSelectedElements, setLocalSelectedElements] =
+    useState<string[]>(selectedElements);
 
-  const toggleSelection = (element: string) => {
-    if (selected.includes(element)) {
-      setSelected(selected.filter((item) => item !== element));
+  const handleElementToggle = (element: string) => {
+    let updatedElements;
+    if (localSelectedElements.includes(element)) {
+      updatedElements = localSelectedElements.filter(
+        (item) => item !== element,
+      );
     } else {
-      setSelected([...selected, element]);
+      updatedElements = [...localSelectedElements, element];
     }
+    setLocalSelectedElements(updatedElements);
+    onSelectElements(updatedElements); // 선택되자마자 부모 컴포넌트에 적용
   };
 
-  const handleApply = () => {
-    onSelect(selected);
-    onClose();
-  };
-
-  const options = [
+  const weatherElements = [
     '강수확률',
     '습도',
     '미세먼지',
@@ -39,39 +40,61 @@ const BottomSheet: React.FC<BottomSheetProps> = ({
   ];
 
   return (
-    <div
-      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end"
-      onClick={onClose}
-    >
-      <div
-        className="bg-white w-full md:w-1/2 lg:w-1/3 rounded-t-lg p-6"
-        style={{ maxHeight: '80%', overflowY: 'auto' }}
-        onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end"
+        onClick={onClose}
       >
-        <h2 className="text-lg font-semibold mb-4">날씨 요소 추가</h2>
-        <div className="grid grid-cols-2 gap-4">
-          {options.map((option) => (
-            <button
-              key={option}
-              onClick={() => toggleSelection(option)}
-              className={`p-2 rounded-md border ${
-                selected.includes(option)
-                  ? 'bg-blue-500 text-white'
-                  : 'bg-gray-200'
-              }`}
-            >
-              {option}
-            </button>
-          ))}
-        </div>
-        <button
-          onClick={handleApply}
-          className="mt-4 p-2 bg-blue-500 text-white rounded-md w-full"
+        <motion.div
+          initial={{ y: '100%' }}
+          animate={{ y: 0 }}
+          exit={{ y: '100%' }}
+          transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+          className="bg-white w-[320px] rounded-t-2xl p-4"
+          onClick={(e) => e.stopPropagation()}
         >
-          적용
-        </button>
-      </div>
-    </div>
+          {/* Handlebar */}
+          <div className="flex justify-center">
+            <div
+              className="w-12 h-1.5 bg-gray-300 rounded-full mb-4 cursor-pointer"
+              onClick={onClose}
+            ></div>
+          </div>
+          <ul className="space-y-2">
+            {weatherElements.map((element) => {
+              const isSelected = localSelectedElements.includes(element);
+              return (
+                <li key={element}>
+                  <label className="flex items-center p-2 border border-transparent rounded-md cursor-pointer ">
+                    <div
+                      onClick={() => handleElementToggle(element)}
+                      className="h-5 w-5"
+                    >
+                      <Image
+                        src={isSelected ? '/checkicon.svg' : '/checkbox.svg'}
+                        alt="checkbox"
+                        width={20}
+                        height={20}
+                      />
+                    </div>
+                    <span
+                      className="ml-3"
+                      style={{ color: isSelected ? '#121212' : '#808080' }}
+                    >
+                      {element}
+                    </span>
+                  </label>
+                </li>
+              );
+            })}
+          </ul>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
