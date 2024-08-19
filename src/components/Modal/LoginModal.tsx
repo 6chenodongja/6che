d74761b6ac81afa48b469c3d86d5b6e64f406323
fjw1010 <fjw_1010@naver.com>
2024-08-19 22:39:30 +0900

    style: 로그인 모달 스타일 수정 텍스트 크기 수정 필요

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -25,16 +25,16 @@ const LoginModalProps = ({ isOpen, onClose }: LoginModalProps) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50">
-      <div className="bg-white bg-opacity-70 rounded-2xl shadow-[0px_0px_2px_0px_rgba(0,0,0,0.05),4px_4px_20px_0px_rgba(0,0,0,0.05)] flex flex-col backdrop-blur-sm max-w-full h-auto px-10 pt-10 pb-6 z-50">
-        <div className="flex flex-col text-center">
+      <div className="bg-white bg-opacity-70 rounded-2xl shadow-[0px_0px_2px_0px_rgba(0,0,0,0.05),4px_4px_20px_0px_rgba(0,0,0,0.05)] backdrop-blur-sm w-[281px] max-w-full h-auto p-6 z-50rounded-lg px-10 pt-10 pb-6 z-50">
+        <div className="text-center">
           <button
             title="x-icon"
             onClick={onClose}
-            className="flex justify-end rounded-full hover:bg-[#4d4d4d]/50"
+            className="absolute top-2 right-2 rounded-full flex justify-end items-end p-2 hover:bg-black/5 focus:bg-black/10"
           >
             <Image src="/x.svg" alt="close" width={24} height={24} />
           </button>
-          <h2 className="text-[18px] font-semibold leading-[130%] tracking-[-0.36px] font-sans text-black-700">
+          <h2 className="text-[18px] text-center font-semibold leading-[130%] tracking-[-0.36px] font-sans text-black-700">
             로그인이 필요한 기능입니다
           </h2>
         </div>
