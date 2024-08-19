43723bcca1199d87f4c0b9610aa5d69506113f28
eunkyum <silverkyum27@naver.com>
2024-08-19 22:16:36 +0900

    fix: 주석 제거

--- src/app/(providers)/(root)/postform/page.tsx
+++ src/app/(providers)/(root)/postform/page.tsx
@@ -1,38 +1,3 @@
-// 'use client';
-
-// import React from 'react';
-// import PostForm from './PostForm';
-
-// function PostFormPage() {
-//   const [isDesktop, setIsDesktop] = React.useState(false);
-
-//   React.useEffect(() => {
-//     const handleResize = () => {
-//       setIsDesktop(window.innerWidth >= 769);
-//     };
-
-//     handleResize();
-
-//     window.addEventListener('resize', handleResize);
-
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
-
-//   return (
-//     <div className="bg-[#fafafa] flex flex-col justify-center items-center w-full min-h-screen p-0 m-0">
-//       {/* {isDesktop && } */}
-
-//       <main className="bg-[#fafafa] flex flex-col justify-center items-center w-full min-h-screen p-0 m-0">
-//         <PostForm />
-//       </main>
-
-//       {/* {isDesktop && } */}
-//     </div>
-//   );
-// }
-
-// export default PostFormPage;
-
 'use client';
 
 import React from 'react';
--- src/app/(providers)/(root)/thermometer-style/page.tsx
+++ src/app/(providers)/(root)/thermometer-style/page.tsx
@@ -1,20 +1,3 @@
-// 'use client';
-
-// import React from 'react';
-// import ThermometerStyle from './ThermometerStyle';
-
-// function Page() {
-//   return (
-//     <div className="container bg-neutral-50 flex flex-col justify-center  items-center w-full min-h-screen">
-//       <main className="container bg-neutral-50 flex flex-col justify-center  items-center w-full min-h-screen">
-//         <ThermometerStyle />
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default Page;
-
 'use client';
 
 import React from 'react';
