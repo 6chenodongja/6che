b89f07a7465f0b449e146c7b5b66233d6b1e25b5
soyoung <fjw_1010@naver.com>
2024-08-20 12:20:02 +0900

    style: 앱 바텀시트 ui, 웹 프로필 선택 ui, singUpDone ui 문제 해결 완료 후 적용 잘 되는 상태, 현재 회원가입 gmail 제외 나머지 도메인 가입이 정상적으로 되는 상태

--- src/app/(providers)/(root)/(auth)/signUpDone/_components/BottomSheet.tsx
+++ src/app/(providers)/(root)/(auth)/signUpDone/_components/BottomSheet.tsx
@@ -73,7 +73,7 @@ const BottomSheet = () => {
 
   return (
     <div
-      className={`md:hidden flex fixed inset-0 z-50 items-end justify-center w-full ${isVisible ? 'bg-black bg-opacity-50' : 'bg-transparent'} transition-opacity duration-500`}
+      className={`md:hidden flex fixed inset-0 z-50 items-end w-full ${isVisible ? 'bg-black bg-opacity-50' : 'bg-transparent'} transition-opacity duration-500`}
       style={{
         visibility: isVisible ? 'visible' : 'hidden',
         opacity: isVisible ? 1 : 0,
@@ -83,30 +83,35 @@ const BottomSheet = () => {
         initial={{ y: '500%' }}
         animate={{ y: isClosing ? '500%' : 0 }}
         transition={{ type: 'spring', stiffness: 200, damping: 40 }}
-        className="flex flex-col justify-center rounded-t-2xl py-[30px] px-4 bg-white/70 backdrop-blur-[10px] lg:w-full lg:h-full lg:bg-white"
+        className="fixed bottom-0 w-full flex flex-col justify-center rounded-t-2xl py-[30px] px-4 bg-white/70 backdrop-blur-[10px] lg:w-full lg:h-full lg:bg-white"
       >
         <div className="flex flex-col gap-0.5">
           <div className="text-center">
             <h1 className="text-[18px] font-semibold li">프로필 선택</h1>
           </div>
           <form className="w-full h-full" onSubmit={handleSubmit}>
-            <div className="grid grid-cols-5 h-full py-4 pl-[19px] pr-4 rounded-2xl w-full cursor-pointer">
+            <div className="grid grid-cols-5 h-full gap-y-2 py-4 pl-[19px] pr-4 rounded-2xl w-full cursor-pointer">
               {profileIcons.map((icon, index) => (
-                <Image
+                <div
                   key={index}
-                  src={icon}
-                  alt={`profile-icon-${index}`}
-                  width={22}
-                  height={22}
-                  className={`border-2 rounded-md p-1 w-full ${profileIcon === icon ? 'border-[#7EC5FF]/60' : 'border-transparent'}`}
-                  onClick={() => handleProfileIconSelect(icon)}
-                />
+                  className="w-[42px] h-[42px] justify-self-center"
+                >
+                  <Image
+                    key={index}
+                    src={icon}
+                    alt={`profile-icon-${index}`}
+                    width={22}
+                    height={22}
+                    className={`border-2 rounded-md p-1 w-full bg-white ${profileIcon === icon ? 'border-[#7EC5FF]/60' : 'border-transparent'}`}
+                    onClick={() => handleProfileIconSelect(icon)}
+                  />
+                </div>
               ))}
             </div>
             <div className="flex justify-center items-center pt-1">
               <button
                 type="submit"
-                className="font-sans py-[16px] px-3 w-full rounded-lg shadow-sm text-center font-semibold text-[white] bg-[#121212] hover:bg-[#5EB0FFCC]"
+                className="font-sans h-[49px] px-3 w-full rounded-lg shadow-sm text-center font-semibold text-[white] bg-[#121212] hover:bg-[#5EB0FFCC]"
               >
                 선택 완료
               </button>
--- src/app/(providers)/(root)/(auth)/signUpDone/_components/ProfileSelect.tsx
+++ src/app/(providers)/(root)/(auth)/signUpDone/_components/ProfileSelect.tsx
@@ -70,45 +70,45 @@ const ProfileSelect = () => {
 
   return (
     <main
-      className={`hidden absolute left-0 top-0 flex-col justify-center items-center lg:w-full lg:h-full lg:flex ${isClosing ? 'lg:!hidden' : ''}`}
+      className={`hidden absolute top-0 left-0 flex-col justify-center items-center rounded-t-2xl py-14 px-10 md:bg-white md:w-full md:h-full md:flex ${isClosing ? 'md:!hidden' : ''}`}
     >
-      <div className="flex flex-col justify-center rounded-t-2xl py-[30px] px-4 bg-white/70 backdrop-blur-[10px] lg:w-full lg:h-full lg:bg-white">
-        <div className="flex flex-col w-full gap-0.5">
-          <div className="text-center">
-            <h1 className="text-[18px] font-semibold">프로필 선택</h1>
-            <h2 className="text-[14px] font-normal">
-              프로필은 마이페이지에서 변경할 수 있습니다
-            </h2>
-          </div>
-          <form className="w-full h-full" onSubmit={handleSubmit}>
-            <div className="grid grid-cols-6 h-full py-4 pl-[19px] pr-4 rounded-2xl w-full cursor-pointer">
-              {profileIcons.map((icon, index) => (
-                <div
-                  key={index}
-                  className="w-[52px] h-[52px] justify-self-center"
-                >
-                  <Image
-                    src={icon}
-                    alt={`profile-icon-${index}`}
-                    width={34}
-                    height={34}
-                    className={`border-2 rounded-md p-1 w-full ${profileIcon === icon ? 'border-[#7EC5FF]/60' : 'border-transparent'}`}
-                    onClick={() => handleProfileIconSelect(icon)}
-                  />
-                </div>
-              ))}
-            </div>
-            <div className="flex justify-center items-center pt-[134px]">
-              <button
-                type="submit"
-                className="font-sans w-full py-[16px] px-3 rounded-lg shadow-sm text-center font-semibold text-[white] bg-[#121212] hover:bg-[#5EB0FFCC]"
-              >
-                선택 완료
-              </button>
-            </div>
-          </form>
+      {/* <div className="flex flex-col justify-center rounded-t-2xl py-14 px-10 bg-white/70 backdrop-blur-[10px] md:w-full md:h-full md:bg-white"> */}
+      <div className="flex flex-col w-full h-full justify-between">
+        <div className="text-center">
+          <h1 className="text-[18px] font-bold ">프로필 선택</h1>
+          <h2 className="text-[14px] font-normal">
+            프로필은 마이페이지에서 변경할 수 있습니다
+          </h2>
         </div>
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-6 py-4 pl-[19px] pr-4 gap-y-8 rounded-2xl w-full cursor-pointer mb-[134px]">
+            {profileIcons.map((icon, index) => (
+              <div
+                key={index}
+                className="w-[52px] h-[52px] justify-self-center"
+              >
+                <Image
+                  src={icon}
+                  alt={`profile-icon-${index}`}
+                  width={34}
+                  height={34}
+                  className={`border-2 rounded-md p-1 w-full ${profileIcon === icon ? 'border-[#7EC5FF]/60' : 'border-transparent'}`}
+                  onClick={() => handleProfileIconSelect(icon)}
+                />
+              </div>
+            ))}
+          </div>
+          {/* <div className="flex justify-center items-center"> */}
+          <button
+            type="submit"
+            className="font-sans w-full py-[16px] px-3 rounded-lg shadow-sm text-center font-semibold text-[white] bg-[#121212] hover:bg-[#5EB0FFCC]"
+          >
+            선택 완료
+          </button>
+          {/* </div> */}
+        </form>
       </div>
+      {/* </div> */}
 
       {/* <button onClick={handleClose}>선택</button> */}
     </main>
